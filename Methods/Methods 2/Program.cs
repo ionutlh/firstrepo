0e3e67f9c9d90a522cf3ffbfb03b4016852bea49
ionutlh <jleahu@gmail.com>
2019-11-16 12:19:27 +0200

    initial commit

---
+++ Methods/Methods 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Methods_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write something:");         
+            string firstNumber = Console.ReadLine();
+
+            Console.WriteLine("Please write something else:");
+            string lastNumber = Console.ReadLine();
+
+            int number1 = int.Parse(firstNumber);
+
+            int number2 = int.Parse(lastNumber);
+
+
+
+            Console.WriteLine("The sum is: " + (number1 + number2)+ " ?");
+        }
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // int sum = Sum();
+            // Console.WriteLine(Sum());
+
+            Sum();
+            int first= 7;
+            int second = 10;
+
+            Console.WriteLine(Multiply(first, second));
+            
+            Multiply(first, second);
+
+            Multiply(4, 5);
+
+            DisplayNumbers(100);
+        }
+
+        static void Sum()
+        {
+            int a = 2;
+            int b = 3;
+
+            int sum = a + b;
+            
+            Console.WriteLine(sum);
+
+        }
+
+        static int Multiply(int a, int b)
+        {
+            return a * b;
+
+
+        }
+
+        static void DisplayNumbers(int n)
+        {
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
